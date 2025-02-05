c6363cced70702d210cb0f64e0d12a15f1886430
Hasanboyman <casanboyy@gmail.com>
2025-02-05 12:46:10 +0500

    navbar

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", alias: { name: "BurgerKing" } },
+    { path: "/", redirect: { name: "BurgerKing" } },
     {
       path: "/",
       component: () => import("../layouts/default.vue"),
